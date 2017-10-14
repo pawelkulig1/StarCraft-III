5082c0e523e51a8e7c2ff60a3d73ebc8a55e189f
pawelkulig1 <pawelkulig1@gmail.com>
2017-10-14 11:37:00 +0200

    Initial Commit

---
+++ StarCraft III/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StarCraft III
+//
+//  Created by Paweł Kulig on 14.10.2017.
+//  Copyright © 2017 Paweł Kulig. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
